cbe761823f97d2b0c44626d76dd42fb0a0ec71de
Bagus <bagus.nurhuda@ui.ac.id>
2022-07-19 17:06:37 +0700

    adding new experiences

--- src/components/InstaFeeds.js
+++ src/components/InstaFeeds.js
@@ -9,6 +9,7 @@ import "../styles/instaFeeds.css"
 
 const InstaFeeds = ({...props}) => {
     const token = 'IGQVJVbWtSX1Rwa0lvUWVESG02MUp0SFoxaDFvZAGJPM2NWZAnRCNThHLUk1eHVLU0c0ZAmJ6S05LTWhaSXZAfYU1OSHNQN2NXbWZAXYXlQR3RVQXRET0NCeDdWUW1zZA1dBTTE5M0g0R0NoekpMV3F0a19GMgZDZD'
+    //beda
     const [feeds, setFeedsData] = useState([]);
     //use useRef to store the latest value of the prop without firing the effect
     const tokenProp = useRef(token);
